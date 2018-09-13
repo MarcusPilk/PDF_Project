b99c6bf07200545ae38f32004c0c284ad73476e1
RichardChester <richard.chester93@gmail.com>
2018-09-13 08:57:56 +0100

    Temporary checking

--- React_PDF/client/src/App.js
+++ React_PDF/client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Table } from 'reactstrap';
-import { Button } from 'reactstrap';
 import MyPDFViewer from './MyPDFViewer';
 
 class App extends Component {
@@ -38,7 +37,7 @@ class App extends Component {
 
           </div>
         
-          <button id="myBtn">Open Sample.pdf</button>
+          <button id="myBtn">Sample.pdf</button>
 
           <div id="myModal" class="modal">
 
